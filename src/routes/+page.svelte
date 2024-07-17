cc78e50ead6b5e6d51a08f17013117637f17808a
lucas.subli <github@lucasf.com.br>
2024-07-17 11:54:40 -0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<h1>Apply does not seem to work</h1>
+
+<p class="text-red-700">I am using inline</p>
+
+<p class="nice-color">I am using apply</p>
+
+<style>
+	.nice-color {
+		@apply text-red-700;
+	}
+</style>
